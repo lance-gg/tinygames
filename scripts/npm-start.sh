9c942cbb004945a59b176f136ca7021d5e9405b9
namel <fourvelocity@gmail.com>
2018-11-24 18:57:57 +0200

    run wiggle on different port

--- scripts/npm-start.sh
+++ scripts/npm-start.sh
@@ -5,9 +5,10 @@ cd /var/games/tinygames
 # arg 1: game name
 # arg 2: port name
 run-game () {
+    pkill -f $1 || true
     cd /var/games/tinygames/$1
     PORT=$2 npm start >$1.out 2>$1.err &
 }
 
 run-game asteroids 3002
-run-game wiggle 3003
+run-game wiggle 3004
