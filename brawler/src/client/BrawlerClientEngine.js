24a718293196a58ed218dc59815f0e7bff11bd74
namel <fourvelocity@gmail.com>
2019-01-08 21:56:47 +0200

    brawler try-again button

--- brawler/src/client/BrawlerClientEngine.js
+++ brawler/src/client/BrawlerClientEngine.js
@@ -7,6 +7,19 @@ export default class BrawlerClientEngine extends ClientEngine {
     constructor(gameEngine, options) {
         super(gameEngine, options, BrawlerRenderer);
 
+        // show try-again button
+        gameEngine.on('objectDestroyed', (obj) => {
+            if (obj.playerId === gameEngine.playerId) {
+                document.body.classList.add('lostGame');
+                document.querySelector('#tryAgain').disabled = false;
+            }
+        });
+
+        // restart game
+        document.querySelector('#tryAgain').addEventListener('click', () => {
+            window.location.reload();
+        });
+
         this.controls = new KeyboardControls(this);
         this.controls.bindKey('up', 'up', { repeat: true } );
         this.controls.bindKey('down', 'down', { repeat: true } );
@@ -14,5 +27,6 @@ export default class BrawlerClientEngine extends ClientEngine {
         this.controls.bindKey('right', 'right', { repeat: true } );
         this.controls.bindKey('space', 'space');
     }
+
 
 }
